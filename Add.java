e4ad0f7f91d1c189f6bff1807ce768ef209a9430
rahulh.dollop <rahulh.dollop@gmail.com>
2024-02-03 23:20:58 +0530

    first_commit

---
+++ Add.java
@@ -0,0 +1,7 @@
+class Add{
+    public static void main(String args[]){
+        Integer firstNumber = 10;
+        Integer secondNumber = 20;
+        System.out.println(firstNumber+secondNumber);
+    }
+}
